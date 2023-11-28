b9c511869d03a974e4f36dec64b395e8dab359ec
Faruk Saritepeci <56247988+faruksaritepeci@users.noreply.github.com>
2023-11-28 17:58:00 +0300

    Added User records and authorization

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -8,11 +9,14 @@ import (
 	"math/rand"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/golang-jwt/jwt/v5"
 	graphql "github.com/graph-gophers/graphql-go"
 	"github.com/graph-gophers/graphql-go/relay"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
@@ -21,6 +25,7 @@ const MAXATTEMPT = 5
 const SHORTLEN = 7
 const ALLOWEDCHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 const PRELINKADDRESS = "https://iugo.tech/"
+const TESTKEY = "testprivatekey"
 
 // GraphQL
 const schemaFile = "schema.graphql"
@@ -68,19 +73,57 @@ type DataBaseWrapper struct {
 type DataBase interface {
 	createUrlRow(*UrlDuo) error
 	getUrlRowByShortUrl(string) (*UrlDuo, error)
+	updateUrlDuo(*UrlDuo) error
 	getAllUrlDuos(int) (*[]UrlDuo, error)
+	createUser(*User) error
+	getUserById(uint) (*User, error)
+	getUserByIdWithUrlDuos(uint) (*User, error)
+	getUserByEmail(string) (*User, error)
 }
 
 type Logic interface {
-	generateShortLink(string) (string, error)
+	generateShortLink(uint, string) (string, error)
 	getLongLink(string) (string, error)
+	updateShortLink(uint, string, string) error
+	createUser(string, string, string) (uint, error)
+	getUser(uint) (*User, error)
+	getUserWithUrlDuos(uint) (*User, error)
+	loginUser(string, string) (string, error)
 }
 
 type UrlDuo struct {
 	ShortUrl   string     `gorm:"short_url"`
 	LongUrl    string     `gorm:"long_url"`
 	ExpireDate *time.Time `gorm:"expire_date"`
+	UserID     uint
 } // sql.NullTime
+
+type User struct {
+	gorm.Model
+	Username   string `gorm:"username"`
+	HashedPass string `gorm:"hashed_pass"`
+	Email      string `gorm:"email"`
+	UrlDuos    []UrlDuo
+}
+
+type UserForm struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type UserView struct {
+	ID       uint     `json:"id"`
+	Username string   `json:"username"`
+	Email    string   `json:"email"`
+	UrlDuos  []UrlDuo `json:"urlDuos"`
+}
+
+type contextKey int
+
+const (
+	userIDKey contextKey = iota
+)
 
 func main() {
 	// Dependency Injection
@@ -110,20 +153,25 @@ func main() {
 	// Routing
 	r := chi.NewRouter()
 	r.Get("/{urlParam}", c.getLongLink)
-	r.Post("/", c.generateShortLink)
-	r.Handle("/query", &relay.Handler{Schema: schema})
+	r.Post("/", AuthMiddleWare(http.HandlerFunc(c.generateShortLink)))
+	r.Put("/{urlParam}", AuthMiddleWare(http.HandlerFunc(c.updateShortLink)))
+	r.Get("/user/{urlParam}", AuthMiddleWare(http.HandlerFunc(c.getUser)))
+	r.Post("/user", c.createUser)
+	r.Post("/login", c.loginUser)
+	r.Handle("/query", AuthMiddleWare(&relay.Handler{Schema: schema}))
 	r.Get("/sandbox", http.HandlerFunc(enableApolloSandbox))
 	http.ListenAndServe(":80", r)
 }
 
 // TODO: Cut down parameters for "Logic" methods (e.g. Use string only param for getLongLink(shortURL string))
-func (logic SimpleLogic) generateShortLink(longLink string) (string, error) {
+func (logic SimpleLogic) generateShortLink(userID uint, longLink string) (string, error) {
 	shortLink := calculateShortLink(longLink)
 	expireDate := time.Now().Add(time.Hour * 1).UTC()
 	urlRow := UrlDuo{
 		ShortUrl:   shortLink,
 		LongUrl:    longLink,
 		ExpireDate: &expireDate,
+		UserID:     userID,
 	}
 	err := logic.db.createUrlRow(&urlRow)
 	if err != nil {
@@ -149,6 +197,81 @@ func (logic SimpleLogic) getLongLink(shortLink string) (string, error) {
 	return urlRow.LongUrl, nil
 }
 
+func (logic SimpleLogic) updateShortLink(userID uint, shortLink string, longLink string) error {
+	u, err := logic.db.getUrlRowByShortUrl(shortLink)
+	if err != nil {
+		return err
+	}
+	if u.UserID != userID {
+		return errors.New("unauthorized")
+	}
+	u.LongUrl = longLink
+	err = logic.db.updateUrlDuo(u)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (logic SimpleLogic) createUser(username string, email string, password string) (uint, error) {
+	passHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		logic.l.Println("Failed password generation: " + err.Error())
+		return 0, err
+	}
+	if username == "" || email == "" || password == "" {
+		return 0, errors.New("username, email and password are required")
+	}
+	user := User{
+		Username:   username,
+		Email:      email,
+		HashedPass: string(passHash),
+	}
+	err = logic.db.createUser(&user)
+	if err != nil {
+		return 0, err
+	}
+	return user.ID, nil
+}
+
+func (logic SimpleLogic) getUser(id uint) (*User, error) {
+	user, err := logic.db.getUserById(id)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (logic SimpleLogic) getUserWithUrlDuos(id uint) (*User, error) {
+	user, err := logic.db.getUserByIdWithUrlDuos(id)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (logic SimpleLogic) loginUser(email string, password string) (string, error) {
+	user, err := logic.db.getUserByEmail(email)
+	if err != nil {
+		return "", err
+	}
+	if err := bcrypt.CompareHashAndPassword([]byte(user.HashedPass), []byte(password)); err != nil {
+		return "", err
+	}
+	t := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"iss":    "url-shortener",
+			"sub":    user.Email,
+			"sub_id": user.ID,
+			"exp":    jwt.NewNumericDate(time.Now().Add(1 * time.Hour)),
+		})
+	s, err := t.SignedString([]byte(TESTKEY))
+	if err != nil {
+		return "", err
+	}
+	return s, nil
+}
+
 func (dbw DataBaseWrapper) createUrlRow(urlRow *UrlDuo) error {
 	result := dbw.gormDB.Create(urlRow)
 	if result.Error != nil {
@@ -166,6 +289,15 @@ func (dbw DataBaseWrapper) getUrlRowByShortUrl(shortUrl string) (*UrlDuo, error)
 	return &urlRow, nil
 }
 
+func (dbw DataBaseWrapper) updateUrlDuo(urlRow *UrlDuo) error {
+	// dbw.gormDB.First(urlRow, urlRow.ShortUrl)
+	result := dbw.gormDB.Where("short_url = ?", urlRow.ShortUrl).Save(urlRow)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
 func (dbw DataBaseWrapper) getAllUrlDuos(last int) (*[]UrlDuo, error) {
 	if last < -1 {
 		return nil, errors.New("invalid number for last query")
@@ -178,15 +310,55 @@ func (dbw DataBaseWrapper) getAllUrlDuos(last int) (*[]UrlDuo, error) {
 	return &urlDuoSlice, nil
 }
 
+func (dbw DataBaseWrapper) createUser(u *User) error {
+	result := dbw.gormDB.Create(u)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func (dbw DataBaseWrapper) getUserById(id uint) (*User, error) {
+	var u User
+	result := dbw.gormDB.Where("id = ?", id).First(&u)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &u, nil
+}
+
+func (dbw DataBaseWrapper) getUserByIdWithUrlDuos(id uint) (*User, error) {
+	var u User
+	result := dbw.gormDB.Preload("UrlDuos").Where("id = ?", id).First(&u)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &u, nil
+}
+
+func (dbw DataBaseWrapper) getUserByEmail(email string) (*User, error) {
+	var u User
+	result := dbw.gormDB.Where("email = ?", email).First(&u)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &u, nil
+}
+
 func (c Controller) generateShortLink(w http.ResponseWriter, r *http.Request) {
-	c.l.Printf("Short link request - by:" + r.UserAgent())
+	requestUserID, ok := r.Context().Value(userIDKey).(int)
+	if !ok {
+		http.Error(w, "unexpected error", http.StatusInternalServerError)
+		return
+	}
+	c.l.Printf("UrlDuo update request - by user ID:" + strconv.Itoa(requestUserID))
 	var l linkData
 	err := json.NewDecoder(r.Body).Decode(&l)
 	if err != nil || len(l.Link) == 0 {
 		http.Error(w, "Wrong format: Send JSON with 'link' attribute", http.StatusBadRequest)
 		return
 	}
-	shortLink, err := c.logic.generateShortLink(l.Link)
+	shortLink, err := c.logic.generateShortLink(uint(requestUserID), l.Link)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -203,4 +375,145 @@ func (c Controller) getLongLink(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Write([]byte(longLink))
+}
+
+func (c Controller) updateShortLink(w http.ResponseWriter, r *http.Request) { //TEST
+	requestUserID, ok := r.Context().Value(userIDKey).(int)
+	if !ok {
+		http.Error(w, "unexpected error", http.StatusInternalServerError)
+		return
+	}
+	c.l.Printf("UrlDuo update request - by user ID:" + strconv.Itoa(requestUserID))
+	var l linkData
+	shortLink := chi.URLParam(r, "urlParam")
+	err := json.NewDecoder(r.Body).Decode(&l)
+	if err != nil || len(l.Link) == 0 {
+		http.Error(w, "Wrong format: Send JSON with 'link' attribute", http.StatusBadRequest)
+		return
+	}
+	err = c.logic.updateShortLink(uint(requestUserID), shortLink, l.Link)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write([]byte("UrlDuo updated"))
+}
+
+func (c Controller) getUser(w http.ResponseWriter, r *http.Request) {
+	requestUserID, ok := r.Context().Value(userIDKey).(int)
+	if !ok {
+		http.Error(w, "unexpected error", http.StatusInternalServerError)
+		return
+	}
+	c.l.Printf("User request - by user ID:" + strconv.Itoa(requestUserID))
+	id := chi.URLParam(r, "urlParam")
+	userID, err := strconv.Atoi(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	u, err := c.logic.getUserWithUrlDuos(uint(userID)) // u, err := c.logic.getUser(uint(userID))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	uView := u.ToUserView()
+	json.NewEncoder(w).Encode(uView)
+}
+
+func (c Controller) createUser(w http.ResponseWriter, r *http.Request) {
+	c.l.Printf("Create user request - by:" + r.UserAgent())
+	var u UserForm
+	err := json.NewDecoder(r.Body).Decode(&u)
+	if err != nil || len(u.Username) == 0 || len(u.Email) == 0 || len(u.Password) == 0 {
+		http.Error(w, "Wrong format: Send JSON with 'username', 'email' and 'password' attributes", http.StatusBadRequest)
+		return
+	}
+	id, err := c.logic.createUser(u.Username, u.Email, u.Password)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write([]byte("User created with ID: " + strconv.Itoa(int(id))))
+}
+
+func (c Controller) loginUser(w http.ResponseWriter, r *http.Request) {
+	c.l.Printf("Login user request - by:" + r.UserAgent())
+	var u UserForm
+	err := json.NewDecoder(r.Body).Decode(&u)
+	if err != nil || len(u.Email) == 0 || len(u.Password) == 0 {
+		http.Error(w, "Wrong format: Send JSON with 'email' and 'password' attributes", http.StatusBadRequest)
+		return
+	}
+	token, err := c.logic.loginUser(u.Email, u.Password)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+	response := map[string]interface{}{
+		"token":      token,
+		"expires_in": 3600,
+		"token_type": "Bearer",
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func (u User) ToUserView() UserView {
+	return UserView{
+		ID:       u.ID,
+		Username: u.Username,
+		Email:    u.Email,
+		UrlDuos:  u.UrlDuos,
+	}
+}
+
+func AuthMiddleWare(next http.Handler) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		var b, tokenString string
+		_, err := fmt.Sscan(authHeader, &b, &tokenString)
+		if err != nil {
+			http.Error(w, "Invalid Bearer", http.StatusUnauthorized)
+			return
+		}
+		if tokenString == "" {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
+			return []byte(TESTKEY), nil
+		})
+		if err != nil || !token.Valid {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok {
+			http.Error(w, "Invalid Token", http.StatusNonAuthoritativeInfo)
+			return
+		}
+		exp, err := claims.GetExpirationTime()
+		if err != nil {
+			http.Error(w, "Invalid Token", http.StatusNonAuthoritativeInfo)
+			return
+		}
+		if exp.Unix() < jwt.NewNumericDate(time.Now()).Unix() {
+			http.Error(w, "Token expired", http.StatusUnauthorized)
+			return
+
+		}
+		userID, ok := claims["sub_id"].(float64)
+		if !ok {
+			http.Error(w, "Unexpected Error", http.StatusInternalServerError)
+			return
+		}
+		ctx := r.Context()
+		ctx = context.WithValue(ctx, userIDKey, int(userID))
+		r = r.WithContext(ctx)
+		next.ServeHTTP(w, r)
+	})
 }
